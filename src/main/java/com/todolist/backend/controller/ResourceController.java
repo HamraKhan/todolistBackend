c1cc436a1a2be8f3ff18ebc2b219c851136be53e
Hamra Khan <hamra111@gmail.com>
2021-05-07 19:31:42 +0100

    Added spring boot application and implemented the todolist endpoint for retrieving the list of activities.

---
+++ src/main/java/com/todolist/backend/controller/ResourceController.java
@@ -0,0 +1,27 @@
+package com.todolist.backend.controller;
+
+import com.todolist.backend.model.TodoList;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class ResourceController {
+    @GetMapping("/todolist")
+    public ArrayList<TodoList> getTodoList() {
+        ArrayList<TodoList> todoList = new ArrayList<>();
+        todoList.add(TodoList
+                .builder().title("wash car").checked(false).status("ACTIVE").todoDate(1618615255648L).build());
+        todoList.add(TodoList.builder().title("collect package").checked(false).status("ACTIVE").todoDate(1618615433559L).build());
+
+        return todoList;
+    }
+    @GetMapping("/")
+    public @ResponseBody String getDashboard() {
+        return "This is a dashboard";
+    }
+    @GetMapping("/error")
+    public @ResponseBody String getErrorPage() {
+        return "This is a error page";
+    }
+}
